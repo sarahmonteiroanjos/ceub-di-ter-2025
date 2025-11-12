94b06e390987459ad1dbf6a464fe4285be49a748
Sarah Monteiro <sarah.monteiro@dinfor.uniceub>
2025-11-11 21:42:45 -0300

    Ajuste de pastas

--- Sarah Monteiro/Aulas/atividade_to_do/tarefa.js
+++ Sarah Monteiro/Aulas/atividade_to_do/tarefa.js
@@ -8,8 +8,27 @@ window.onload = function () {
         const texto = input.value.trim();
         
         const li = document.createElement("li");
-        li.textContent = texto;
+//Adicionar o valor 
+li.textContent = texto;
 
-        lista.appendChild(li);
-    })
+li.addEventListener("click", () => {
+    li.classList.toggle("concluida")
+})
+
+// Criar um botão de executar
+const btnRemover = this.document.createElement("button")
+btnRemover.textContent = "Remover";
+btnRemover.style.marginLeft = "10px";
+btnRemover.classList.add("fechar");
+btnRemover.addEventListener("click", (e) => {
+//Deletar o item
+li.remove();
+});
+//Adicionar o  item na lista
+lista.appendChild(li);
+//Adicione o botão remover
+li.appendChild(btnRemover)
+input.value = "";
+
+})
 };
